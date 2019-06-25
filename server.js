62e19320a854ab00fed2baf1fc8014da54d84840
Anhar Athoi <anharzihan@gmail.com>
2019-06-25 17:03:56 +1000

    Added cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = new express()
 const mongoose = require('mongoose')
 const PORT = 5000
+const cors = require('cors')
 
 const mongoURI = 'mongodb://localhost/booksdb'
 
 const booksRoutes = require('./routes/booksRoutes')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/', booksRoutes)
